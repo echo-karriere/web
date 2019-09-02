fad33659840c3d2b0a8877dabd626523307105af
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-09-02 12:55:32 +0200

    Add Bootstrap navbar, remove old one

--- src/components/PageWrapper/PageWrapper.tsx
+++ src/components/PageWrapper/PageWrapper.tsx
@@ -1,38 +1,33 @@
-import React from 'react'
-import {Content, Footer, Wrapper} from './style'
-import {NavigationMenu} from "./components/NavigationMenu";
-import { withRouter, RouteComponentProps, Switch, Route } from 'react-router';
+import React from "react";
+import { Content, Footer, Wrapper } from "./style";
+import { withRouter, RouteComponentProps, Switch, Route } from "react-router";
 
-import { routes } from '../../routes';
-import { NoMatch } from '../../pages/no-match';
+import { routes } from "../../routes";
+import { NoMatch } from "../../pages/no-match";
+import { NavBar } from "./components/Navbar";
 
 interface PageWrapperProps extends RouteComponentProps {}
 
 function PageWrapper(props: PageWrapperProps) {
-  
   return (
     <Wrapper>
-      <NavigationMenu/>
+      <NavBar />
       <Content>
         <Switch>
-          {
-            routes.map(route => (
-              <Route
-                key={route.path}
-                path={route.path}
-                exact={route.exact}
-                component={route.component}
-              />
-            ))
-          }
-          <Route component={NoMatch}/>
+          {routes.map(route => (
+            <Route
+              key={route.path}
+              path={route.path}
+              exact={route.exact}
+              component={route.component}
+            />
+          ))}
+          <Route component={NoMatch} />
         </Switch>
       </Content>
-      <Footer>
-        echo - Fagutvalget for informatikk
-      </Footer>
+      <Footer>echo - Fagutvalget for informatikk</Footer>
     </Wrapper>
-  )
+  );
 }
 
-export default withRouter(PageWrapper)
+export default withRouter(PageWrapper);
