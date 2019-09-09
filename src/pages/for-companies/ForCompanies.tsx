6d9e0bcebdd5555c1375d046a9d32213eb0156c1
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-09-10 00:01:54 +0200

    Add content for companies, a small program

--- src/pages/for-companies/ForCompanies.tsx
+++ src/pages/for-companies/ForCompanies.tsx
@@ -4,7 +4,39 @@ import { ContentWrapper } from "../../components/ContentWrapper";
 interface ForCompaniesProps {}
 
 const ForCompanies: React.FC<ForCompaniesProps> = () => {
-  return <ContentWrapper>Kommer snart!</ContentWrapper>;
+  return (
+    <ContentWrapper>
+      <h1>Nyttig information</h1>
+      <h2>Timeplan</h2>
+      <ul>
+        <li>
+          10:15 - 13:00 - Rigging av lokaler. Vi er til stede for å hjelpe til
+          og vi anbefaler dere å møte opp i god tid.
+        </li>
+        <li>13:15 - 14:15 - Lynpresentasjoner </li>
+        <li>14:15 - 18:00 - Standområdet er åpent</li>
+        <li>18:00 - 20:00 - Nedrigging av lokalet</li>
+      </ul>
+      <h2>Praktisk informasjon</h2>
+      <p>
+        Dere har et område på 4m^2 å bruke. Lokalet kommer til å være fullt, så
+        det er ikke mulig å bruke mer plass enn det. Vi skaffer et bord og én
+        strømpadde for dere. Det vil også bli enkel matservering i form av
+        boller.
+      </p>
+      <h2>Kom dere frem</h2>
+      <p>
+        Adressen for arrangementet er: <br />
+        Høyteknologisenteret <br />
+        Thormøhlensgate 55 <br />
+        5008 Bergen
+      </p>
+      <p>
+        Se også universitetets egne guide for å finne frem{" "}
+        <a href="https://www.uib.no/ii/54321/finn-fram-til-oss">her.</a>
+      </p>
+    </ContentWrapper>
+  );
 };
 
 export default ForCompanies;
--- src/pages/program/Program.tsx
+++ src/pages/program/Program.tsx
@@ -4,7 +4,15 @@ import { ContentWrapper } from "../../components/ContentWrapper";
 interface ProgramProps {}
 
 const Program: React.FC<ProgramProps> = () => {
-  return <ContentWrapper>Kommer snart!</ContentWrapper>;
+  return (
+    <ContentWrapper>
+      <h1>Program</h1>
+      <h2>13:15 - 14:15</h2>
+      <p>Lynpresentasjoner i store auditorium</p>
+      <h2>14:14 - 18:00</h2>
+      <p>Stands i hovedfoajeen (oversikt kommer)</p>
+    </ContentWrapper>
+  );
 };
 
 export default Program;
