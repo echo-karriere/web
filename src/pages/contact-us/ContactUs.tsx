c6ff1a4fba60821deb0f7cf40ee12f789a833b5e
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-09-10 00:09:59 +0200

    Create a quick and dirty contact us page

--- src/pages/contact-us/ContactUs.tsx
+++ src/pages/contact-us/ContactUs.tsx
@@ -4,7 +4,14 @@ import { ContentWrapper } from "../../components/ContentWrapper";
 interface ContactUsProps {}
 
 const ContactUs: React.FC<ContactUsProps> = () => {
-  return <ContentWrapper>Kommer snart!</ContentWrapper>;
+  return (
+    <ContentWrapper>
+      <h1>Kontakt oss</h1>
+      <p>
+        Du kan kontakte oss på vår epostadresse: kontakt (at) echokarriere.no.
+      </p>
+    </ContentWrapper>
+  );
 };
 
 export default ContactUs;
