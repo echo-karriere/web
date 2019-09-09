bf4e603c9b5e7c1c999452d7ef9e88ceb323387b
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-09-09 23:41:01 +0200

    Give content more breathing room on desktop

--- src/components/ContentWrapper/ContentWrapper.tsx
+++ src/components/ContentWrapper/ContentWrapper.tsx
@@ -2,6 +2,10 @@ import styled from "styled-components";
 
 const ContentWrapper = styled.div`
   padding: 1rem;
+
+  @media screen and (min-width: 768px) {
+    padding: 1rem 5rem;
+  }
 `;
 
 export default ContentWrapper;
