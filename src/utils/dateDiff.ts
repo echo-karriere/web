890c94517695d4d7d8b1c764971ecf6d5a869ef7
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-09-20 18:48:40 +0200

    Format everything with Prettier for consistency

--- src/utils/dateDiff.ts
+++ src/utils/dateDiff.ts
@@ -1,23 +1,23 @@
 export interface DaysDateDiff {
-    days: number
-    hours: number
-    minutes: number
-    seconds: number
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
 }
 
 export default function calcDateDiff(targetDate: Date): DaysDateDiff {
-    const currentDate = Date.now();
-    const diff = Number(targetDate) - currentDate;
+  const currentDate = Date.now();
+  const diff = Number(targetDate) - currentDate;
 
-    let days = Math.floor((diff / 86400000) % 30);
-    const hours = Math.floor((diff / 3600000) % 24);
-    const minutes = Math.floor((diff / 60000) % 60);
-    const seconds = Math.floor((diff / 1000) % 60);
+  let days = Math.floor((diff / 86400000) % 30);
+  const hours = Math.floor((diff / 3600000) % 24);
+  const minutes = Math.floor((diff / 60000) % 60);
+  const seconds = Math.floor((diff / 1000) % 60);
 
-    return {
-        days,
-        hours,
-        minutes,
-        seconds
-    }
+  return {
+    days,
+    hours,
+    minutes,
+    seconds
+  };
 }
