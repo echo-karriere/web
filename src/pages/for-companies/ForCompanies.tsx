bd1b070fc176a269971f121ce6e5181d27eb4f7c
Marie Heggebakk <marie.heggebakk@gmail.com>
2019-09-23 15:13:05 +0200

    Add networkinformation and refactor for-companies

--- src/pages/for-companies/ForCompanies.tsx
+++ src/pages/for-companies/ForCompanies.tsx
@@ -3,6 +3,7 @@ import { ContentWrapper } from "../../components/ContentWrapper";
 import { Link } from "../../components/Link";
 import { PageTitle } from "../../components/PageWrapper/style";
 import styled from "styled-components";
+import { PageWrapper } from "../../components/PageWrapper";
 
 interface ForCompaniesProps {}
 
@@ -31,17 +32,28 @@ const ForCompanies: React.FC<ForCompaniesProps> = () => {
         <li>14:15 - 18:00 - Standområdet er åpent</li>
         <li>18:00 - 20:00 - Nedrigging av lokalet</li>
       </ul>
-      <h5>Praktisk informasjon</h5>
+      <PageTitle>Praktisk informasjon</PageTitle>
+      <h5>Standområdet</h5>
       <p>
         Dere har et område på 2m * 2m (4m^2) å bruke. Lokalet kommer til å være
         fullt, så det er ikke mulig å bruke mer plass enn det. Vi skaffer et
         bord og én strømpadde for dere. Det vil også bli enkel matservering i
         form av boller.
       </p>
-      <h5>Standkart</h5>
-      <StandWrapper>
-        <StandMap src="/misc/standkart-h19.png" alt="Standkart" />
-      </StandWrapper>
+      <h5>Koble seg til uib-guest-nettet:</h5>
+      <p>
+        Vis trådløse nett på din enhet, og koble til nettet som heter
+        "uib-guest". Enheten vil i noen tilfelle opplyse at tilkobling krever
+        autentisering, og spørre om du vil gå til autentiseringssiden. Eller du
+        kan åpne nettleser på enheten. Du skal da få opp autentiseringssiden
+        første gang du prøver å laste en nettside. Dersom autentiseringssiden
+        ikke laster automatisk, kan du nå den ved å skrive inn{" "}
+        <Link href="https://uib-guest.privnett.uib.no">
+          https://uib-guest.privnett.uib.no
+        </Link>{" "}
+        i adressefeltet i din nettleser, etter at du har valgt uib-guest som
+        nettverk.{" "}
+      </p>
       <h5>Kom dere frem</h5>
       <p>
         Adressen for arrangementet er: <br />
@@ -59,6 +71,11 @@ const ForCompanies: React.FC<ForCompaniesProps> = () => {
           ved å klikke her.
         </Link>
       </p>
+
+      <PageTitle>Standkart</PageTitle>
+      <StandWrapper>
+        <StandMap src="/misc/standkart-h19.png" alt="Standkart" />
+      </StandWrapper>
     </ContentWrapper>
   );
 };
