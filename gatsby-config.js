6cdddb0e39a75d2a67c7e84dea6fa783e93e8cc1
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-03-06 16:41:08 +0100

    Add Netlify configuration, update LICENSE

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
       }
     },
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-styled-components`
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-netlify`
   ]
 };
