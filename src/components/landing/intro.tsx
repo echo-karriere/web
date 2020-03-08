378221f7b4b11733b0826e09427f43264233ac29
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-03-08 15:27:09 +0100

    Add move information to landing page

--- src/components/landing/intro.tsx
+++ src/components/landing/intro.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import styled from "styled-components";
+import { Container } from "../common/container";
+
+import about from "../../assets/about.svg";
+import hiring from "../../assets/hiring.svg";
+import students from "../../assets/students.svg";
+import { math } from "polished";
 
 const Wrapper = styled.div`
   background: url("/images/mountains.svg");
@@ -8,6 +14,7 @@ const Wrapper = styled.div`
   background-repeat: no-repeat;
   background-position: center;
   height: 60vh;
+  margin-bottom: ${props => math(`${props.theme.size.spacing} * 4`)};
 `;
 
 const Hello = styled.div`
@@ -28,6 +35,53 @@ const Hello = styled.div`
   }
 `;
 
+const Art = styled.figure`
+  margin: 0;
+  max-width: ${props => math(`${props.theme.screen.md} / 2`)};
+  width: 100%;
+`;
+
+type GridType = {
+  readonly inverse?: boolean;
+};
+
+const Grid = styled.div<GridType>`
+  display: grid;
+  grid-template-columns: 3fr 2fr;
+  grid-gap: ${props => math(`${props.theme.size.spacing} * 2`)};
+  text-align: right;
+  align-items: center;
+  justify-items: center;
+  margin: ${props => props.theme.size.spacing} 0;
+  ${props =>
+    props.inverse &&
+    `
+    text-align: left;
+    grid-template-columns: 2fr 3fr;
+  `}
+
+  h2 {
+    margin-bottom: 16px;
+  }
+
+  @media (max-width: ${props => props.theme.screen.md}) {
+    grid-template-columns: 1fr;
+    text-align: left;
+
+    &:last-child {
+      margin-bottom: {props => props.theme.size.spacing};
+    }
+
+    ${props =>
+      props.inverse &&
+      `
+        ${Art} {
+          order: 2;
+        }
+    `}
+  }
+`;
+
 const Intro: React.FC = () => {
   const { apiLanding: data } = useStaticQuery(graphql`
     query IntroPage {
@@ -35,6 +89,8 @@ const Intro: React.FC = () => {
         title
         subtitle
         about
+        for_companies
+        for_students
       }
     }
   `);
@@ -47,6 +103,35 @@ const Intro: React.FC = () => {
           <h2>{data.subtitle}</h2>
         </Hello>
       </Wrapper>
+      <Container>
+        <Grid>
+          <div>
+            <h2>Om echo karriere</h2>
+            <div dangerouslySetInnerHTML={{ __html: data.about }} />
+          </div>
+          <Art>
+            <img src={about} />
+          </Art>
+        </Grid>
+        <Grid inverse>
+          <Art>
+            <img src={hiring} />
+          </Art>
+          <div>
+            <h2>For bedrifter</h2>
+            <div dangerouslySetInnerHTML={{ __html: data.for_companies }} />
+          </div>
+        </Grid>
+        <Grid>
+          <div>
+            <h2>For studenter</h2>
+            <div dangerouslySetInnerHTML={{ __html: data.for_students }} />
+          </div>
+          <Art>
+            <img src={students} />
+          </Art>
+        </Grid>
+      </Container>
     </>
   );
 };
