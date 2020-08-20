e848410da8a2a7eb214213c824c319a9e09276ce
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-08-20 18:01:37 +0200

    Revamp menu since it will only open on mobile

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,21 +1,13 @@
-import React, { useState, useRef } from "react";
-import FocusLock from "react-focus-lock";
+import React, { useState } from "react";
 import Burger from "./Burger";
 import Menu from "./Menu";
-import { useOnClickOutside } from "../../hooks/useOnClickOutside";
 
 const Navbar: React.FC = () => {
   const [open, setOpen] = useState(false);
-  const node = useRef<HTMLDivElement>(null);
-
-  useOnClickOutside(node.current, () => setOpen(false));
-
   return (
-    <div ref={node}>
-      <FocusLock disabled={!open}>
-        <Burger open={open} setOpen={setOpen} />
-        <Menu open={open} />
-      </FocusLock>
+    <div>
+      <Burger open={open} setOpen={setOpen} />
+      <Menu open={open} />
     </div>
   );
 };
