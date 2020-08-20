70671bd11551244dc2201a559cdd6b49e198b681
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-08-20 17:23:21 +0200

    Refactor navbar to be CSS-modules

--- src/components/navbar/Burger.tsx
+++ src/components/navbar/Burger.tsx
@@ -1,75 +1,9 @@
-import styled from "styled-components";
 import React from "react";
-import { math } from "polished";
+import classNamesBind from "classnames/bind";
 
-const StyledBurger = styled.button<{ open: boolean }>`
-  background: transparent;
-  border: none;
-  cursor: pointer;
-  display: flex;
-  flex-direction: column;
-  height: 2rem;
-  justify-content: space-around;
-  padding: 0;
-  position: absolute;
-  right: 2rem;
-  top: ${({ theme }) => math(`${theme.size.spacing} / 1.5`)};
-  width: 2rem;
-  z-index: 10;
+import styles from "./Burger.module.scss";
 
-  @media screen and (prefers-reduced-motion: reduce) {
-    span {
-      background: ${({ theme }) => theme.color.grey};
-      border-radius: 10px;
-      height: 0.25rem;
-      position: relative;
-      transform-origin: 1px;
-      transition: none;
-      width: 2rem;
-
-      :first-child {
-        transform: ${({ open }) => (open ? "rotate(45deg)" : "rotate(0)")};
-      }
-
-      :nth-child(2) {
-        opacity: ${({ open }) => (open ? "0" : "1")};
-        transform: ${({ open }) =>
-          open ? "translateX(20px)" : "translateX(0)"};
-      }
-
-      :nth-child(3) {
-        transform: ${({ open }) => (open ? "rotate(-45deg)" : "rotate(0)")};
-      }
-    }
-  }
-
-  span {
-    background: ${({ theme }) => theme.color.grey};
-    border-radius: 10px;
-    height: 0.25rem;
-    position: relative;
-    transform-origin: 1px;
-    transition: all 0.3s linear;
-    width: 2rem;
-
-    :first-child {
-      transform: ${({ open }) => (open ? "rotate(45deg)" : "rotate(0)")};
-    }
-
-    :nth-child(2) {
-      opacity: ${({ open }) => (open ? "0" : "1")};
-      transform: ${({ open }) => (open ? "translateX(20px)" : "translateX(0)")};
-    }
-
-    :nth-child(3) {
-      transform: ${({ open }) => (open ? "rotate(-45deg)" : "rotate(0)")};
-    }
-  }
-
-  @media screen and (min-width: ${({ theme }) => theme.screen.md}) {
-    display: none;
-  }
-`;
+const cx = classNamesBind.bind(styles);
 
 interface Props {
   open: boolean;
@@ -78,16 +12,16 @@ interface Props {
 
 const Burger: React.FC<Props> = ({ open, setOpen }) => {
   return (
-    <StyledBurger
-      open={open}
+    <button
+      className={styles.burger}
       onClick={() => setOpen(!open)}
       aria-label="Toggle menu"
       aria-expanded={open}
     >
-      <span />
-      <span />
-      <span />
-    </StyledBurger>
+      <span className={cx("line", { firstOpen: open, firstClosed: !open })} />
+      <span className={cx("line", { secondOpen: open, secondClosed: !open })} />
+      <span className={cx("line", { thirdOpen: open, thirdClosed: !open })} />
+    </button>
   );
 };
 
--- src/components/navbar/MenuItem.tsx
+++ src/components/navbar/MenuItem.tsx
@@ -1,36 +1,8 @@
 import React, { ReactNode } from "react";
-import styled from "styled-components";
 import { Link } from "gatsby";
 import downArrow from "../../assets/angle-down-solid.svg";
 import upArrow from "../../assets/angle-up-solid.svg";
-
-const NavLink = styled(Link)`
-  align-self: center;
-  color: ${({ theme }) => theme.color.textColor};
-  font-size: 2rem;
-  font-weight: bold;
-  letter-spacing: 0.5rem;
-  padding: 2rem 0;
-  text-align: center;
-  text-decoration: none;
-  text-transform: uppercase;
-  transition: color 0.3s linear;
-
-  &:visited {
-    color: ${({ theme }) => theme.color.textColor};
-  }
-
-  @media screen and (min-width: ${({ theme }) => theme.screen.md}) {
-    font-size: 1rem;
-    font-weight: normal;
-    letter-spacing: normal;
-    text-transform: none;
-  }
-`;
-
-const Icon = styled.img`
-  width: 12px;
-`;
+import styles from "./MenuItem.module.scss";
 
 interface Props {
   title: string;
@@ -39,17 +11,19 @@ interface Props {
 }
 
 export const DownArrow: React.FC = () => {
-  return <Icon src={downArrow} alt="Expand icon" />;
+  return <img src={downArrow} alt="Expand icon" className={styles.icon} />;
 };
 
 export const UpArrow: React.FC = () => {
-  return <Icon src={upArrow} alt="Minimize icon" />;
+  return <img src={upArrow} alt="Minimize icon" className={styles.icon} />;
 };
 
 const MenuItem: React.FC<Props> = ({ title, to, children = undefined }) => {
   return (
     <>
-      <NavLink to={to}>{title}</NavLink>
+      <Link to={to} className={styles.link}>
+        {title}
+      </Link>
       {children && children}
     </>
   );
