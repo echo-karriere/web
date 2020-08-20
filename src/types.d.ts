209386ec15b04481b40e2008a70af0eb82cfa3bc
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-08-20 15:02:55 +0200

    Add Sass configuration, start transitioning to (S)CSS-modules

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,3 +2,8 @@ declare module "*.svg" {
   const content: string;
   export default content;
 }
+
+declare module "*.scss" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
