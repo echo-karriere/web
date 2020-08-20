58db0eb52647b0cf5ad129c908c51c1b0bb6cca9
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-08-20 17:23:21 +0200

    Refactor navbar to be CSS-modules

--- src/components/navbar/Menu.tsx
+++ src/components/navbar/Menu.tsx
@@ -1,52 +1,11 @@
 import React from "react";
-import styled from "styled-components";
 import MenuItem from "./MenuItem";
 import useNavigationData, { NavItem } from "../../queries/useNavigationData";
+import classNamesBind from "classnames/bind";
 
-export const StyledMenu = styled.nav<{ open: boolean }>`
-  background: ${({ theme }) => theme.color.backgroundcolor};
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  justify-content: center;
-  left: 0;
-  padding: 2rem;
-  position: absolute;
-  text-align: left;
-  top: 0;
-  transform: ${({ open }) => (open ? "translateX(0)" : "translateX(-100%)")};
-  transition: transform 0.3s ease-in-out;
+import styles from "./Menu.module.scss";
 
-  @media (max-width: ${({ theme }) => theme.screen.sm}) {
-    width: 100%;
-  }
-
-  @media screen and (min-width: ${({ theme }) => theme.screen.md}) {
-    display: flex;
-    flex-direction: row;
-    height: auto;
-    left: initial;
-    position: initial;
-    top: initial;
-    transform: none;
-  }
-`;
-
-const MenuItems = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-
-  @media screen and (min-width: ${({ theme }) => theme.screen.md}) {
-    flex-direction: row;
-
-    > * {
-      &:not(:last-child) {
-        margin-right: ${({ theme }) => theme.size.spacing};
-      }
-    }
-  }
-`;
+const cx = classNamesBind.bind(styles);
 
 interface Props {
   open: boolean;
@@ -56,13 +15,16 @@ const Menu: React.FC<Props> = ({ open }) => {
   const navigation = useNavigationData();
 
   return (
-    <StyledMenu open={open} aria-hidden={!open}>
-      <MenuItems>
+    <nav
+      className={cx("menu", { menuOpen: open, menuClosed: !open })}
+      aria-hidden={!open}
+    >
+      <div className={styles.item}>
         {navigation.map((nav: NavItem) => (
           <MenuItem title={nav.title} to={nav.link} key={nav.link} />
         ))}
-      </MenuItems>
-    </StyledMenu>
+      </div>
+    </nav>
   );
 };
 export default Menu;
