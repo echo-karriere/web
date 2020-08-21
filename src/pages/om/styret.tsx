cf6f8128450f1eee1f9423a0b1371fadeb819101
Marie Heggebakk <marie.heggebakk@gmail.com>
2020-08-21 22:36:18 +0200

    Add card for each member in the committee

---
+++ src/pages/om/styret.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
+import Layout from "../../components/common/Layout";
+import SEO from "../../components/common/Seo";
+import { Container } from "../../components/common/Container";
+import styles from "./styret.module.scss";
+
+interface User {
+  first_name: string;
+  last_name: string;
+  title: string;
+  email: string;
+}
+
+interface Node {
+  node: {
+    user: User;
+  };
+}
+
+interface Response {
+  allApiCommittee: {
+    edges: Node[];
+  };
+}
+
+const Committee: React.FC = () => {
+  const response = useStaticQuery<Response>(graphql`
+    query MyQuery {
+      allApiCommittee {
+        edges {
+          node {
+            user {
+              first_name
+              last_name
+              title
+              email
+              avatar
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  console.log(JSON.stringify(response, null, 2));
+
+  const {
+    allApiCommittee: { edges: members },
+  } = response;
+
+  console.log(JSON.stringify(members, null, 2));
+
+  return (
+    <Layout>
+      <SEO title="Styret" />
+      <Container>
+        <h1>Styret</h1>
+        <div className={styles.members}>
+          {members.map(({ node: { user: member } }) => (
+            <div className={styles.card} key={member.last_name}>
+              <h2 className={styles.name}>{member.first_name}</h2>
+              <h2 className={styles.name}>{member.last_name}</h2>
+              <h4 className={styles.title}>{member.title}</h4>
+              <p className={styles.email}>{member.email}</p>
+            </div>
+          ))}
+        </div>
+      </Container>
+    </Layout>
+  );
+};
+
+export default Committee;
