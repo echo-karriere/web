e12747756f1bdb214fe7134325070934b304756b
Marie Heggebakk <marie.heggebakk@gmail.com>
2020-08-21 17:57:26 +0200

    Fix typo

--- src/pages/for-studenter/sok-til-styret.tsx
+++ src/pages/for-studenter/sok-til-styret.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import Layout from "../../components/common/Layout";
 import SEO from "../../components/common/Seo";
 import { Container } from "../../components/common/Container";
-import CommiteeApplication from "../../components/forms/CommiteeApplicationForm";
+import CommitteeApplication from "../../components/forms/CommiteeApplicationForm";
 
 const Index: React.FC = () => (
   <Layout>
     <SEO title="Søk til styret" />
     <Container>
-      <CommiteeApplication />
+      <CommitteeApplication />
     </Container>
   </Layout>
 );
