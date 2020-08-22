a805045d4cca57d2d0a438a194a589364a49deb5
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-08-22 16:18:52 +0200

    Refactor menu so dropdown works

--- src/components/navbar/MenuItem.tsx
+++ src/components/navbar/MenuItem.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { Link } from "gatsby";
 import styles from "./MenuItem.module.scss";
+import { DownArrow, UpArrow } from "./Menu";
 
 interface Props {
   title: string;
   to: string;
 }
+
+interface IconProps extends Props {
+  open: boolean;
+}
+
+export const MenuItemIcon: React.FC<IconProps> = ({ title, to, open }) => {
+  return (
+    <Link to={to} className={styles.link}>
+      {title} {open ? <UpArrow /> : <DownArrow />}
+    </Link>
+  );
+};
 
 const MenuItem: React.FC<Props> = ({ title, to }) => {
   return (
