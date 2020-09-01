08b1af470ffb4a3d3629c4105228c7c7eba01b32
Marie Heggebakk <marie.heggebakk@gmail.com>
2020-09-01 18:00:35 +0200

    Add social metadata

--- src/components/common/Seo.tsx
+++ src/components/common/Seo.tsx
@@ -12,44 +12,30 @@ export function SEO({ description: desc, title: t }: Props): JSX.Element {
 
   const title = t ?? meta.title;
   const description = desc ?? meta.description;
+  const image = `${meta.siteUrl}/icons/icon-192x192.png`;
 
   return (
     <Helmet
       htmlAttributes={{ lang: "no" }}
       title={`${title} | ${meta.title}`}
       meta={[
-        {
-          name: `description`,
-          content: description,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: description,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: meta.title,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: description,
-        },
+        { name: "name", content: title || meta.title },
+        { name: `description`, content: description },
+        { name: "image", content: image },
+
+        { name: `twitter:card`, content: `summary` },
+        { name: "twitter:site", content: `echo karriere` },
+        { name: `twitter:title`, content: title },
+        { name: `twitter:description`, content: description },
+        { name: `twitter:creator`, content: "echo karriere" },
+        { name: "twitter:image", content: image },
+
+        { property: "og:type", content: "website" },
+        { property: "og:title", content: title },
+        { property: "og:url", content: meta.siteUrl },
+        { property: "og:image", content: image },
+        { property: "og:description", content: description },
+        { property: "og:site_name", content: `echo karriere` },
       ]}
     />
   );
