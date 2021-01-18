4cc59276bf1967d5ba34f643ed59f23195df83cf
Marie Heggebakk <marie.heggebakk@gmail.com>
2021-01-18 15:07:38 +0100

    Update hero for event February 12th 2021

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -31,7 +31,7 @@ export function Hero(): JSX.Element {
             <h1 className={styles.title}>{data.title}</h1>
             <h2 className={styles.subtitle}>{data.subtitle}</h2>
             <p>
-              <StyledLink to="/utsatt-til-2021/">Utsatt til 2021 ➔</StyledLink>
+              <StyledLink to="/pamelding/">Les mer her ➔</StyledLink>
             </p>
           </div>
         </div>
