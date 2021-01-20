227e30f9b8cbead1071f0dc0ed15096cf52b46eb
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-01-20 19:08:44 +0100

    Refactor contact us form to use Tailwind

--- src/components/forms/ContactUs.tsx
+++ src/components/forms/ContactUs.tsx
@@ -5,8 +5,6 @@ import axios from "axios";
 import * as yup from "yup";
 import { isDev } from "../../config";
 
-import styles from "./Forms.module.scss";
-
 type FormData = {
   name: string;
   email: string;
@@ -65,74 +63,129 @@ export function ContactUs(): JSX.Element {
 
   if (submitted) {
     return (
-      <div className={styles.centered}>
-        <h1>Takk!</h1>
-        <h2>Du hører fra oss så snart som mulig.</h2>
+      <div className="bg-white py-16 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-24 h-96 flex flex-col justify-center items-center">
+        <div className="relative max-w-xl mx-auto">
+          <div className="text-center">
+            <h1 className="text-3xl font-bold text-gray-900 sm:text-4xl">
+              Takk!
+            </h1>
+            <h2>Du hører fra oss så snart som mulig.</h2>
+          </div>
+        </div>
       </div>
     );
   }
 
   if (error) {
     return (
-      <div className={styles.centered}>
-        <h1>Noe gikk galt :(</h1>
-        <h2>Prøv igjen senere</h2>
+      <div className="bg-white py-16 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-24 h-96 flex flex-col justify-center items-center">
+        <div className="relative max-w-xl mx-auto">
+          <div className="text-center">
+            <h1 className="text-3xl font-bold text-gray-900 sm:text-4xl">
+              Noe gikk galt :(
+            </h1>
+            <h2>Prøv igjen senere</h2>
+          </div>
+        </div>
       </div>
     );
   }
 
   return (
-    <>
-      <h2>Kontakt oss</h2>
-      <form onSubmit={handleSubmit(submitForm)}>
-        <section>
-          <fieldset className={styles.fieldset}>
-            <legend className={styles.legend}>Kontakt</legend>
-            <label htmlFor="name_id">
-              Navn
-              {errors.name && (
-                <span className={styles.error}> {errors.name.message}</span>
-              )}
-            </label>
-            <input
-              type="text"
-              name="name"
-              id="name_id"
-              ref={register({ required: true })}
-            />
-
-            <label htmlFor="email_id">
-              Epost
-              {errors.email && (
-                <span className={styles.error}> {errors.email.message}</span>
-              )}
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email_id"
-              ref={register({ required: true })}
-            />
-
-            <label htmlFor="message" className={styles.label}>
-              Melding
-              {errors.message && (
-                <span className={styles.error}> {errors.message.message}</span>
-              )}
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              rows={4}
-              cols={25}
-              ref={register}
-            />
-            <div>
-              <button type="submit">Send melding</button>
+    <div className="bg-white py-16 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-24">
+      <div className="relative max-w-xl mx-auto">
+        <div className="text-center">
+          <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">
+            Kontakt oss
+          </h2>
+          <p className="mt-4 text-lg leading-6 text-gray-500">
+            Send oss gjerne en melding dersom dere har spørsmål eller vil gjerne
+            komme i kontakt.
+          </p>
+        </div>
+        <div className="mt-12 ">
+          <form
+            className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
+            onSubmit={handleSubmit(submitForm)}
+          >
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="name_id"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Navn
+                {errors.name && (
+                  <span className="text-red-600 text-sm float-right">
+                    {errors.name.message}
+                  </span>
+                )}
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  name="name"
+                  id="name_id"
+                  autoComplete="full-name"
+                  className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                  ref={register({ required: true })}
+                />
+              </div>
             </div>
-          </fieldset>
-        </section>
-      </form>
-    </>
+
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="email_id"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Epost
+                {errors.email && (
+                  <span className="text-red-600 text-sm float-right">
+                    {errors.email.message}
+                  </span>
+                )}
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email_id"
+                autoComplete="email"
+                className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                ref={register({ required: true })}
+              />
+            </div>
+
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="message"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Melding
+                {errors.message && (
+                  <span className="text-red-600 text-sm float-right">
+                    {errors.message.message}
+                  </span>
+                )}
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                rows={4}
+                cols={25}
+                className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                ref={register}
+              />
+            </div>
+            <div className="mt-4 sm:col-span-2">
+              <button
+                className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                type="submit"
+              >
+                Kontakt oss
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
