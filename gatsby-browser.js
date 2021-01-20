19c324d5725facec229beb3688bd56531c0f4ef8
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-01-20 14:05:55 +0100

    Remove global styling, use Bitter/Cambo as font instead of Inter

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,3 @@
 import "./src/styles/tailwind.css";
-import "@fontsource/inter";
-import "sanitize.css";
 import "typeface-bitter";
 import "typeface-cambo";
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ["./src/**/*.{ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Bitter", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
