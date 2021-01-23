4855d831ec036d3baf2686528d7337d738acb78a
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-01-23 18:59:45 +0100

    Remove tracking-tight, Cambo/Bitter works best with some breath

--- src/components/landing/Intro.tsx
+++ src/components/landing/Intro.tsx
@@ -32,7 +32,7 @@ export function Intro(): JSX.Element {
       <div className="py-16 bg-gray-50 overflow-hidden lg:py-24">
         <div className="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
           <div className="relative">
-            <h2 className="text-center text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            <h2 className="text-center text-3xl leading-8 font-extrabold text-gray-900 sm:text-4xl">
               {data.content_header}
             </h2>
             <p className="mt-4 max-w-3xl mx-auto text-center text-xl text-gray-500">
@@ -42,7 +42,7 @@ export function Intro(): JSX.Element {
 
           <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
             <div className="relative">
-              <h3 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
+              <h3 className="text-2xl font-extrabold text-gray-900 sm:text-3xl">
                 Om echo karriere
               </h3>
               <div
@@ -137,7 +137,7 @@ export function Intro(): JSX.Element {
           <div className="relative mt-12 sm:mt-16 lg:mt-24">
             <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
               <div className="lg:col-start-2">
-                <h3 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
+                <h3 className="text-2xl font-extrabold text-gray-900 sm:text-3xl">
                   For studenter
                 </h3>
                 <div
@@ -230,7 +230,7 @@ export function Intro(): JSX.Element {
 
           <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
             <div className="relative">
-              <h3 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
+              <h3 className="text-2xl font-extrabold text-gray-900 sm:text-3xl">
                 For bedrifter
               </h3>
               <div
--- src/pages/nyheter.tsx
+++ src/pages/nyheter.tsx
@@ -46,7 +46,7 @@ export default function News(): JSX.Element {
         <div className="bg-white pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
           <div className="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl">
             <div>
-              <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
+              <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
                 Nyheter
               </h2>
               <div className="mt-3 sm:mt-4 lg:grid lg:grid-cols-2 lg:gap-5 lg:items-center">
