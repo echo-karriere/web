49f727469e1834a676c8022411b6961e76386340
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-01-23 18:19:53 +0100

    Extract colors to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,23 @@ module.exports = {
       center: true,
     },
     extend: {
+      colors: {
+        c1: "#fcc000",
+        c2: "#fed97a",
+        c3: "#ffebbc",
+        c4: "#a7d0da",
+        c5: "#52afbe",
+        c6: "#049fb2",
+        c7: "#0090a5",
+        c8: "#008195",
+        c9: "#373736",
+        c10: "#383836",
+        brand: "#2568ba",
+        brandDark: "#205aa2",
+        brandBlue: "#007bff",
+        brandGrey: "#4d4d4d",
+        brandGreyLight: "#b3b3b3",
+      },
       fontFamily: {
         sans: ["Bitter", ...defaultTheme.fontFamily.sans],
       },
