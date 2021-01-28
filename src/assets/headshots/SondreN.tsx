1cd49781f9ec5bae79eb0a933a7c4372e1ca8f59
Marie Heggebakk <marie.heggebakk@gmail.com>
2021-01-28 17:12:59 +0100

    [CH-58]: Added Sondre N headshot

---
+++ src/assets/headshots/SondreN.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import headshot from "./sondreN.jpg";
+
+export const SondreNHeadshot = (
+  <img
+    className="object-cover shadow-lg rounded-lg"
+    alt="Sondre Nilsen"
+    src={headshot}
+  />
+);
--- src/assets/headshots/index.ts
+++ src/assets/headshots/index.ts
@@ -1,3 +1,7 @@
 import { MarieHeadshot } from "./Marie";
+import { SondreNHeadshot } from "./SondreN";
 
-export const headshots = new Map([["Marie Heggebakk", MarieHeadshot]]);
+export const headshots = new Map([
+  ["Marie Heggebakk", MarieHeadshot],
+  ["Sondre Nilsen", SondreNHeadshot],
+]);
