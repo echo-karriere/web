c3ad013d2df38e77a9717aa44d2b077508669d97
Marie Heggebakk <marie.heggebakk@gmail.com>
2021-01-28 17:01:40 +0100

    [CH-58]: Created committee page

---
+++ src/assets/headshots/Marie.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import headshot from "./marie.jpg";
+
+export const MarieHeadshot = (
+  <img
+    className="object-cover shadow-lg rounded-lg"
+    alt="Marie Heggebakk"
+    src={headshot}
+  />
+);
