c908adfc92a2448bb2dd3a878cba363f4bcfd5a7
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-02-04 16:09:25 +0100

    Link to schedule from the main page and create a schedule page

---
+++ src/pages/timeplan.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "gatsby";
+import { Layout, SEO, Container, Content, Schedule } from "../components";
+
+export default function FourOhFour(): JSX.Element {
+  return (
+    <Layout>
+      <SEO
+        title="Timeplan digital karrieredag 2021"
+        description="Hva er planen for karrieredagen 2021?"
+      />
+      <Container>
+        <Content title="Timeplan digital karrieredag 2021">
+          <p>
+            Vil du vite hva som skjer hvor og når? Se en oversikt over
+            timeplanen her, og klikk deg inn på det som skjer! Husk også å se på{" "}
+            <Link to="/jobb/">jobbene</Link> og{" "}
+            <Link to="/karrieredagen-2021">bedriftene som deltar.</Link>
+          </p>
+        </Content>
+        <div className="relative py-4 bg-white overflow-hidden">
+          <div className="relative px-4 sm:px-6 lg:px-8">
+            <Schedule />
+          </div>
+        </div>
+      </Container>
+    </Layout>
+  );
+}
