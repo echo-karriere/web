47aa9569b385a9585a16cd32844d60ad6b189f9c
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-02-12 09:40:58 +0100

    Publish live page

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -54,7 +54,7 @@ export function Hero(): JSX.Element {
                       to="/live/"
                       className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-c5 hover:bg-c6 md:py-4 md:text-lg md:px-10"
                     >
-                      Delta her
+                      Delta her nå
                     </Link>
                   </div>
                   <div className="mt-3 sm:mt-0 sm:ml-3">
