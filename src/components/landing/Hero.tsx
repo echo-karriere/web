c1af818d141ba54ec33d04b7257f6e7a38ae1d82
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-06-09 17:32:47 +0200

    Update hero URL to point to registration form

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -48,7 +48,7 @@ export function Hero(): JSX.Element {
                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                   <div className="rounded-md shadow">
                     <Link
-                      to="/nyheter/2021/pamelding-til-2021/"
+                      to="/for-bedrifter/pamelding/"
                       className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-c5 hover:bg-c6 md:py-4 md:text-lg md:px-10"
                     >
                       Påmelding
