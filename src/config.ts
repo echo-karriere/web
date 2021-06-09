2acc793c951a52edbd04f365459348edcd923668
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-06-09 17:37:31 +0200

    Update isDev function to also trigger on websites that are not www.echokarriere.no

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,3 @@
-export const isDev = process.env.NODE_ENV === "development";
+export const isDev =
+  process.env.NODE_ENV === "development" ||
+  window.location.origin != "https://www.echokarriere.no/";
