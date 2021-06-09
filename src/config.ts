51ac5a5b06392ffc17fe151a10fa5fc69d323021
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-06-09 17:42:51 +0200

    Check if in browser too for isDev function

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
+export const isBrowser = typeof window !== "undefined";
+
 export const isDev =
   process.env.NODE_ENV === "development" ||
-  window.location.origin != "https://www.echokarriere.no/";
+  (isBrowser && window.location.origin != "https://www.echokarriere.no/");
