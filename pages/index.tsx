1b77fded3d409755016b3a5d42b76901be3ddda8
Sondre Nilsen <sondre@eons.io>
2021-08-14 16:21:45 +0200

    Fix some SEO, build issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
-import { Intro, Layout, SEO } from "@/components";
+import { Intro, Layout } from "@/components";
 
 export default function Index(): JSX.Element {
   return (
     <Layout>
-      <SEO />
       <Intro />
     </Layout>
   );
--- utils/seo.ts
+++ utils/seo.ts
@@ -5,6 +5,8 @@ export const defaultSEOConfig: DefaultSeoProps = {
   description: "echo karriere er en karrieredag for IT-studenter i Bergen.",
   canonical: "https://www.echokarriere.no",
   openGraph: {
+    type: "website",
+    locale: "no_NB",
     url: "https://www.echokarriere.no",
     title: "echo karriere",
     description: "echo karriere er en karrieredag for IT-studenter i Bergen.",
