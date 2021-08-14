0aeb6c5f15c544ba0f12a6b7851fd070a61a74ef
Sondre Nilsen <sondre@eons.io>
2021-08-14 16:21:45 +0200

    Fix some SEO, build issues

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Head, Html, Main, NextScript } from "next/document";
 export default class MyDocument extends Document {
   render(): JSX.Element {
     return (
-      <Html lang="en">
+      <Html lang="no">
         <Head>
           <link rel="icon" href="/favicon.png" />
           <link rel="alternate icon" type="image/png" href="/icon.png" sizes="512x512" />
