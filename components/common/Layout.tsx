581e5ba38b03a834c36e1452501e28cfca59b740
Sondre Nilsen <sondre@eons.io>
2021-08-14 16:28:45 +0200

    Update layout component to have title/description metadata

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,15 +1,28 @@
+import { NextSeo } from "next-seo";
 import { ReactNode } from "react";
 
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 
 interface Props {
+  title: string;
+  description: string;
   children: ReactNode;
 }
 
-export function Layout({ children }: Props): JSX.Element {
+export function Layout({ title, description, children }: Props): JSX.Element {
   return (
     <>
+      <NextSeo
+        title={title}
+        description={description}
+        openGraph={{
+          type: "website",
+          title: title,
+          description: description,
+          site_name: "echo karriere",
+        }}
+      />
       <Header />
       {children}
       <Footer />
