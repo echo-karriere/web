09a75ee15a0f35df1bf44fc39e3f9b886ed90136
Sondre Nilsen <sondre@eons.io>
2021-08-14 16:28:45 +0200

    Update layout component to have title/description metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { Intro, Layout } from "@/components";
 
 export default function Index(): JSX.Element {
   return (
-    <Layout>
+    <Layout title="Hjem" description="echo karriere er en karrieredag for IT-studenter i Bergen.">
       <Intro />
     </Layout>
   );
