ae70e76f17b7599455e06e25944f3741dc15d0ed
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-08-16 17:03:18 +0200

    Add page about the committee, [slug]s for students/companies

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,5 @@
-export const hello = "Hello!";
+import path from "path";
+
+export const DATA_DIR = path.join(process.cwd(), "data");
+
+export const COMMITTEE_DATA = path.join(DATA_DIR, "committee.json");
