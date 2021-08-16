62733a6a17fd7a4fb3f32b11b65e416e06d3d5d3
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-08-16 16:12:18 +0200

    Add 404 page, remove old SEO component

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,8 @@
-import { Content, Layout, SEO } from "../components";
+import { Content, Layout } from "@/components";
 
 export default function FourOhFour(): JSX.Element {
   return (
-    <Layout>
-      <SEO
-        title="404: Ikke funnet"
-        description="Vi kunne dessverre ikke finne det du var på jakt etter."
-      />
+    <Layout title="404" description="Vi kunne dessverre ikke finne det du var på jakt etter.">
       <main className="flex items-center flex-col justify-center h-96 p-4">
         <Content title="404: Vi finner ikke siden din :(">
           <p>Gå tilbake eller prøvd på nytt.</p>
