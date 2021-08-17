04864512796b43bbbce395e41a473e36815a7363
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-08-17 15:17:42 +0200

    Use custom link in MDX components

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,3 +1,16 @@
 import { ComponentMap } from "mdx-bundler/client";
+import Link from "next/link";
+import React from "react";
 
-export const components: ComponentMap = {};
+const CustomLink = ({ as, href, ...rest }: { as?: string; href: string; [key: string]: unknown }): JSX.Element => {
+  return (
+    <Link as={as} href={href}>
+      {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
+      <a {...rest} />
+    </Link>
+  );
+};
+
+export const components: ComponentMap = {
+  a: CustomLink as React.ComponentType<Record<string, unknown>>,
+};
