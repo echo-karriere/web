62f309462b782e0048472d6d49b46b957dfc794f
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-08-19 23:01:59 +0200

    Fix job titles overflowing the container

--- components/event/jobs/Job.tsx
+++ components/event/jobs/Job.tsx
@@ -68,8 +68,8 @@ export const Job = ({
         <Image src={logo} alt={company} className="w-full h-full object-contain sm:w-full sm:h-full" />
       </div>
       <div className="flex-1 p-4 space-y-2 flex flex-col">
-        <h3 className="text-sm font-medium text-lg text-gray-900 mx-auto truncate">
-          <a href={link}>
+        <h3 className="flex min-w-0 text-sm font-medium text-lg text-gray-900 mx-auto max-w-xs">
+          <a href={link} className="inline-block truncate">
             <span aria-hidden="true" className="absolute inset-0" />
             {name}
           </a>
