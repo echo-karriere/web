a3daea343fe16b0888981bfbe44fcb0446171816
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-08-19 14:18:25 +0200

    Add aspect-ratio package for Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,5 +27,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio")],
 };
