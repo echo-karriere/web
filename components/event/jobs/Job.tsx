a54fdb15339ce8960807e12f17ef84d27329ee44
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-08-19 14:20:25 +0200

    Update design for job overview page

--- components/event/jobs/Job.tsx
+++ components/event/jobs/Job.tsx
@@ -1,11 +1,15 @@
+import Image from "next/image";
+
 import { FilterBy } from "./Selects";
 
-export type JobType = "full" | "part" | "summer";
+export type JobType = "full" | "part" | "summer" | "other";
 
 export interface JobProps {
   name: string;
   company: string;
+  logo: StaticImageData;
   link: string;
+  description: string;
   location: string | null;
   type: JobType;
   deadline: string | null;
@@ -19,6 +23,8 @@ export const jobType = (type: JobType): string => {
       return "Deltid";
     case "summer":
       return "Sommerjobb";
+    case "other":
+      return "Annet";
   }
 };
 
@@ -26,11 +32,13 @@ export const Job = ({
   name,
   company,
   location,
+  logo,
+  description,
   type,
   deadline,
   link,
   filters,
-}: JobProps & { filters: FilterBy }): JSX.Element => {
+}: JobProps & { filters: FilterBy }): JSX.Element | null => {
   const show = (): boolean => {
     let valid = true;
     if (filters.type) valid = type === filters.type;
@@ -38,77 +46,35 @@ export const Job = ({
     if (filters.location) valid = valid && location === filters.location;
     return valid;
   };
+
+  if (!show()) return null;
+
   return (
-    <li className={show() ? "" : "hidden"}>
-      <a href={link} className="block hover:bg-gray-50" target="_blank" rel="noreferrer noopener">
-        <div className="px-4 py-4 sm:px-6">
-          <div className="flex items-center justify-between">
-            <p className="text-sm font-medium text-c6 truncate">{name}</p>
-            <div className="ml-2 flex-shrink-0 flex">
-              <p className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-c2 text-c8">
-                {jobType(type)}
-              </p>
-            </div>
-          </div>
-          <div className="mt-2 sm:flex sm:justify-between">
-            <div className="sm:flex">
-              <p className="flex items-center text-sm text-gray-500">
-                <svg
-                  className="flex-shrink-0 mr-1.5 h-5 w-5 text-c5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
-                </svg>
-                {company}
-              </p>
-              {location && (
-                <p className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0 sm:ml-6">
-                  <svg
-                    className="flex-shrink-0 mr-1.5 h-5 w-5 text-c5"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                  {location}
-                </p>
-              )}
-            </div>
-            <div className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
-              <svg
-                className="flex-shrink-0 mr-1.5 h-5 w-5 text-c5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              {deadline ? (
-                <p>
-                  Frist til
-                  <time dateTime="2020-01-07"> {deadline}</time>
-                </p>
-              ) : (
-                <p>Fortløpende</p>
-              )}
-            </div>
-          </div>
+    <div className="group relative bg-white border border-gray-200 rounded-lg flex flex-col overflow-hidden">
+      <div className="aspect-w-3 aspect-h-4 group-hover:opacity-75 sm:aspect-none flex justify-center items-center p-2 h-36">
+        <Image src={logo} alt={company} className="w-full h-full object-contain sm:w-full sm:h-full" />
+      </div>
+      <div className="flex-1 p-4 space-y-2 flex flex-col">
+        <h3 className="text-sm font-medium text-lg text-gray-900 mx-auto truncate">
+          <a href={link}>
+            <span aria-hidden="true" className="absolute inset-0" />
+            {name}
+          </a>
+        </h3>
+        <div className="flex flex justify-between">
+          <p className="text-sm font-bold text-gray-900">Frist</p>
+          <p className="text-sm text-gray-600">{deadline ?? "N/A"}</p>
         </div>
-      </a>
-    </li>
+        <div className="flex flex justify-between">
+          <p className="text-sm font-bold text-gray-900">Sted</p>
+          <p className="text-sm text-gray-600">{location ?? "Flere"}</p>
+        </div>
+        <div className="flex flex justify-between">
+          <p className="text-sm font-bold text-gray-900">Stilling</p>
+          <p className="text-sm text-gray-600">{jobType(type)}</p>
+        </div>
+        <p className="text-sm text-gray-500">{description}</p>
+      </div>
+    </div>
   );
 };
--- components/event/jobs/Jobs.tsx
+++ components/event/jobs/Jobs.tsx
@@ -1,52 +1,38 @@
 import { useEffect, useState } from "react";
 
-import { shuffleArray } from "../../../utils";
+import { shuffleArray } from "@/utils";
+
+import { JOBS } from "../../../data/jobs";
 import { FilterBy, Job, JobProps } from ".";
-import { Selects } from "./Selects";
 
 export const Jobs = (): JSX.Element => {
-  const [jobs, setJobs] = useState<JobProps[]>([]);
-  const [filter, setFilter] = useState<FilterBy>({
+  const [jobs, setJobs] = useState<JobProps[]>(JOBS);
+  const [filter] = useState<FilterBy>({
     type: undefined,
     location: undefined,
     company: undefined,
   });
 
   useEffect(() => {
-    shuffleArray(jobs);
-    setJobs(jobs);
+    const copy = [...jobs];
+    shuffleArray(copy);
+    setJobs(copy);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <div className="py-8 xl:py-10 max-w-3xl lg:max-w-5xl xl:grid xl:grid-cols-3">
-      <div className="xl:col-span-2 xl:border-r xl:border-gray-200">
-        <div>
-          <aside className="my-8 flex flex-col items-center justify-center xl:hidden">
-            <Selects jobs={jobs} filter={filter} setFilter={setFilter} />
-          </aside>
-          {jobs.length === 0 && <p>Mer informasjon kommer</p>}
-          <div className="bg-white shadow overflow-hidden sm:rounded-md md:mr-8">
-            <ul className="divide-y divide-gray-200">
-              {jobs.map((node) => (
-                <Job
-                  key={node.link}
-                  name={node.name}
-                  company={node.company}
-                  location={node.location}
-                  type={node.type}
-                  deadline={node.deadline}
-                  link={node.link}
-                  filters={filter}
-                />
-              ))}
-            </ul>
-          </div>
-        </div>
+    <div>
+      {/*<aside className="my-8 flex flex-col items-center justify-center xl:hidden">*/}
+      {/*  <Selects jobs={jobs} filter={filter} setFilter={setFilter} />*/}
+      {/*</aside>*/}
+      <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
+        {jobs.map((job) => (
+          <Job key={job.name} filters={filter} {...job} />
+        ))}
       </div>
-      <aside className="hidden xl:block xl:pl-8">
-        <Selects jobs={jobs} filter={filter} setFilter={setFilter} />
-      </aside>
+      {/*<aside className="hidden xl:block xl:pl-8">*/}
+      {/*  <Selects jobs={jobs} filter={filter} setFilter={setFilter} />*/}
+      {/*</aside>*/}
     </div>
   );
 };
