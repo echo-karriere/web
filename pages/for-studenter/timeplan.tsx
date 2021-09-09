efc2b33e5fad87603c8f3e12ca652fd7a05043e7
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-09-09 22:13:08 +0200

    Fix text disappearing on mobile

--- pages/for-studenter/timeplan.tsx
+++ pages/for-studenter/timeplan.tsx
@@ -77,7 +77,7 @@ export default function SchedulePage(): JSX.Element {
                     tab.current
                       ? "border-indigo-500 text-indigo-600"
                       : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
-                    "w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm",
+                    "py-4 px-1 text-center border-b-2 font-medium text-sm",
                   )}
                   aria-current={tab.current ? "page" : undefined}
                 >
