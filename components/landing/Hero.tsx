419a77828486b20412150e4b47f41a9cf6e508e1
Sondre Nilsen <sondre@eons.io>
2021-09-29 17:23:21 +0200

    Update hero content

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -25,11 +25,10 @@ export function Hero(): JSX.Element {
                 <h1 className="text-4xl font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                   <span className="text-c8 inline">echo karriere</span>
                   <br />
-                  <span className="block text-c10 xl:inline">17. september 2021</span>
+                  <span className="block text-c10 xl:inline">2021</span>
                 </h1>
                 <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                  Nok en gang er det karrieredag i Bergen med bedrifter fra hele landet for alle IT-studenter i
-                  Bergensområdet.
+                  echo karriere ble arrangert 17. september 2021, takk for i år!
                 </p>
                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                   <div className="rounded-md shadow">
