bf85f27f95b980eb72cb6eadc0bfe780bb135789
helges1 <sebhelg@gmail.com>
2022-04-05 15:01:22 +0200

    🇺🇦

--- components/common/Header/Desktop/DesktopMenu.tsx
+++ components/common/Header/Desktop/DesktopMenu.tsx
@@ -14,8 +14,8 @@ interface Props {
 export function DesktopMenu({ mobileButtonClicked }: Props): JSX.Element {
   return (
     <>
-      <div className="border-8 border-blue-600"></div>
-      <div className="border-8 border-yellow-300"></div>
+      <div className="bg-blue-500 h-4" />
+      <div className="bg-yellow-300 h-4" />
       <div className="max-w-7xl mx-auto px-4 sm:px-0">
         <div className="flex justify-between items-center border-b-2 border-gray-100 pt-6 md:justify-start md:space-x-10">
           <div>
