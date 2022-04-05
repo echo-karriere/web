f505b894cd734dd11d9e7bcf38839ccc24bb777f
helges1 <sebhelg@gmail.com>
2022-04-05 14:44:17 +0200

    🇺🇦 and minor changes

--- components/common/Header/Desktop/DesktopMenu.tsx
+++ components/common/Header/Desktop/DesktopMenu.tsx
@@ -13,59 +13,63 @@ interface Props {
 
 export function DesktopMenu({ mobileButtonClicked }: Props): JSX.Element {
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-0">
-      <div className="flex justify-between items-center border-b-2 border-gray-100 pt-6 md:justify-start md:space-x-10">
-        <div>
-          <Link href="/">
-            <a>
-              <Image className="w-auto" src={logo} alt="echo karriere logo" width={160} height={70} />
-            </a>
-          </Link>
-        </div>
-        <div className="-mr-2 -my-2 md:hidden">
-          <button
-            onClick={mobileButtonClicked}
-            className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none"
-          >
-            <span className="sr-only">Åpne meny</span>
-            <svg
-              className="h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
+    <>
+      <div className="border-8 border-blue-600"></div>
+      <div className="border-8 border-yellow-300"></div>
+      <div className="max-w-7xl mx-auto px-4 sm:px-0">
+        <div className="flex justify-between items-center border-b-2 border-gray-100 pt-6 md:justify-start md:space-x-10">
+          <div>
+            <Link href="/">
+              <a>
+                <Image className="w-auto" src={logo} alt="echo karriere logo" width={160} height={70} />
+              </a>
+            </Link>
+          </div>
+          <div className="-mr-2 -my-2 md:hidden">
+            <button
+              onClick={mobileButtonClicked}
+              className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none"
             >
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-          </button>
-        </div>
-        <nav className="hidden md:flex space-x-10 pl-8">
-          {navigation.map((item) => (
-            <div key={item.title} className="relative">
-              <MenuDropdown title={item.title}>
-                <div className="absolute -ml-4 mt-3 transform px-2 w-screen max-w-sm sm:px-0 lg:ml-0 lg:left-1/2 md:-translate-x-1/2">
-                  <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                    <Menu.Items>
-                      <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                        {item.items.map((child) => (
-                          <MenuItem
-                            title={child.title}
-                            to={child.to}
-                            description={child.description}
-                            key={child.to}
-                            Icon={child.Icon}
-                          />
-                        ))}
-                      </div>
-                    </Menu.Items>
+              <span className="sr-only">Åpne meny</span>
+              <svg
+                className="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+            </button>
+          </div>
+          <nav className="hidden md:flex space-x-10 pl-8">
+            {navigation.map((item) => (
+              <div key={item.title} className="relative">
+                <MenuDropdown title={item.title}>
+                  <div className="absolute -ml-4 mt-3 transform px-2 w-screen max-w-sm sm:px-0 lg:ml-0 lg:left-1/2 md:-translate-x-1/2">
+                    <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                      <Menu.Items>
+                        <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                          {item.items.map((child) => (
+                            <MenuItem
+                              title={child.title}
+                              to={child.to}
+                              description={child.description}
+                              key={child.to}
+                              Icon={child.Icon}
+                            />
+                          ))}
+                        </div>
+                      </Menu.Items>
+                    </div>
                   </div>
-                </div>
-              </MenuDropdown>
-            </div>
-          ))}
-        </nav>
+                </MenuDropdown>
+              </div>
+            ))}
+          </nav>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- data/jobs.ts
+++ data/jobs.ts
@@ -14,39 +14,6 @@ export const JOBS: Array<JobProps> = [
     type: "full",
     deadline: null,
   },
-  {
-    name: "Internship",
-    company: "Kongsberg Gruppen",
-    link: "https://www.kongsberg.com/careers/vacancies/summer-internship-2022/",
-    logo: ATTENDEE_LOGOS.kongsberg,
-    description:
-      "Throughout the summer internship we promise close supervision from our experienced co-workers whilst working on challenging and innovating tasks and attaining relevant work experience. Our Summer Student Programme is primarily intended for those of you undertaking degrees in science and technology across several of our locations. You will also benefit from unique networking opportunities through social activities after work hours.",
-    location: ["Oslo", "Kongsberg", "Bergen", "Trondheim"],
-    type: "summer",
-    deadline: new Date(Date.parse("2022-02-28")),
-  },
-  {
-    name: "Consultant (Graduate 2022)",
-    company: "Avo Consulting",
-    link: "https://karriere.avoconsulting.no/jobs/1310309-consultant-graduate-2022",
-    logo: ATTENDEE_LOGOS.avo,
-    description:
-      "Avo er et oppstartsselskap; men vi er et oppstartsselskap med muskler. På bare fem år har vi vokst fra å være en håndfull mennesker til å bli nesten 100 ansatte. Dette har vi klart fordi vi er annerledes, og det har vi tenkt til å fortsette med å være. Kall oss gjerne en slags rebell i konsulentbransjen.  Har du Avo-faktor og et ønske om å skape episke løsninger med oss? Søk da vel!",
-    location: ["Bergen"],
-    type: "full",
-    deadline: new Date(Date.parse("2022-03-01")),
-  },
-  {
-    name: "Consultant - Developer (Graduate 2022)",
-    company: "Avo Consulting",
-    link: "https://karriere.avoconsulting.no/jobs/1306250-consultant-developer-graduate-2022",
-    logo: ATTENDEE_LOGOS.avo,
-    description:
-      "Avo er et oppstartsselskap; men vi er et oppstartsselskap med muskler. På bare fem år har vi vokst fra å være en håndfull mennesker til å bli nesten 100 ansatte. Dette har vi klart fordi vi er annerledes, og det har vi tenkt til å fortsette med å være. Kall oss gjerne en slags rebell i konsulentbransjen.  Har du Avo-faktor og et ønske om å skape episke løsninger med oss? Søk da vel!",
-    location: ["Bergen"],
-    type: "full",
-    deadline: new Date(Date.parse("2022-03-01")),
-  },
 ];
 
 export const JOB_LOCATIONS: Array<string> = [
