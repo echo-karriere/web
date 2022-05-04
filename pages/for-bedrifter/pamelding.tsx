0a290909941e542eb32e64cbfdd09eedbef33629
helges1 <sebhelg@gmail.com>
2022-05-04 15:24:33 +0200

    Hosting meta picture on GitHub

--- pages/for-bedrifter/pamelding.tsx
+++ pages/for-bedrifter/pamelding.tsx
@@ -6,7 +6,10 @@ export default function Index(): JSX.Element {
       title="Påmelding 2022"
       description="Ønsker du og din bedrift å delta på echo karriere i september? Påmeldingen er nå åpen! En fantastisk mulighet til å vise frem bedriften for IT-studenter i Bergensområdet."
     >
-      <meta property="og:image" content="https://i.ibb.co/C9nr7wk/cover.png" />
+      <meta
+        property="og:image"
+        content="https://raw.githubusercontent.com/echo-karriere/echo-karriere-static/main/meta_thumbnails/echo_pamelding.png"
+      />
       <Container>
         <RegistrationForm />
       </Container>
