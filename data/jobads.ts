081fe87afedce27e6b8e818091a0a231ec650277
helges1 <sebhelg@gmail.com>
2022-05-05 22:42:06 +0200

    Added job from tietoevry

--- data/jobads.ts
+++ data/jobads.ts
@@ -1,12 +1,14 @@
 import marinit from "../content/images/logos/jobads/marinit.jpg";
+import tietoevry from "../content/images/logos/jobads/tietoevry.jpg";
 
 export interface Jobads {
   company: string;
   logo: StaticImageData;
 }
 
-export type JOB_NAMES = "marinit";
+export type JOB_NAMES = "marinit" | "tietoevry";
 
 export const JOB_LOGOS: Record<JOB_NAMES, StaticImageData> = {
   marinit: marinit,
+  tietoevry: tietoevry,
 };
--- data/jobs.ts
+++ data/jobs.ts
@@ -6,13 +6,24 @@ export const JOBS: Array<JobProps> = [
   {
     name: "IT Support Engineer",
     company: "Marin IT AS",
-    link: "	https://www.linkedin.com/jobs/view/3040848919",
+    link: "https://www.linkedin.com/jobs/view/3040848919",
     logo: JOB_LOGOS.marinit,
     description:
       "As a Support Technician you will contribute to customer satisfaction by assisting end-users in the daily operation of our services. You will be joining a global team, that supports customers in all continents and oceans.",
     location: "Bergen",
     type: "full",
     deadline: null,
+  },
+  {
+    name: "Graduate 2022",
+    company: "Tietoevry",
+    link: "https://tieto.wd3.myworkdayjobs.com/Tieto_Referral_Site/job/Norway-Fornebu/We-are-looking-for-tech-graduates-to-our-specialist-track-in-Norway--Student-Activities_R97120",
+    logo: JOB_LOGOS.tietoevry,
+    description:
+      "We are now recruiting graduates for the fall of 2022 in Norway who want to be a part of shaping the future today! Become a part of our team consisting of 24,000 experts globally, helping businesses and societies meet their full potential – while you are also reaching yours.",
+    location: ["Oslo", "Bergen", "Trondheim", "Stavanger"],
+    type: "full",
+    deadline: new Date(Date.parse("2022-06-23")),
   },
 ];
 
