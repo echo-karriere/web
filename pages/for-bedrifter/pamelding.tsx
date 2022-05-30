ef531ee62d8eff8ff6b0f73d920933592b05e63a
helges1 <sebhelg@gmail.com>
2022-05-30 19:07:49 +0200

    Changes in pamelding

--- pages/for-bedrifter/pamelding.tsx
+++ pages/for-bedrifter/pamelding.tsx
@@ -19,18 +19,10 @@ export default function Index(): JSX.Element {
             <div>
               <h2 className="text-center text-3xl font-bold text-gray-900 sm:text-4xl">Påmelding echo karriere 2022</h2>
               <p className="mt-4 text-lg leading-6 text-gray-500">
-                Vi har nå stengt påmeldingen. Ønsker dere å delta kan det være at vi vil ha mulighet til å motta noen
-                bedrifter kun torsdag 8. september. Ta kontakt med oss på mail{" "}
+                Påmeldingen for echo Karriere 2022 er nå stengt. Ved eventuelle spørsmål, send e-post til{" "}
                 <a className="underline" href="mailto:kontakt@echokarriere.no">
                   kontakt@echokarriere.no
                 </a>
-                , så kan vi se hva vi kan få til. Dere finner invitasjonen{" "}
-                <Link href="/files/invitation.pdf">
-                  <a target="_blank" className="underline">
-                    her
-                  </a>
-                </Link>
-                .
               </p>
             </div>
           </div>
